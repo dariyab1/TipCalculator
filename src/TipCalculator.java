53d23405d557df4b677daf9c12ed6998bb33b92c
dariyab1 <dariyab@nycstudents.net>
2022-09-28 09:24:35 -0400

    first commit

---
+++ src/TipCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        System.out.println("Please enter bill in dollars and cents:");
+        double bill=input.nextDouble();
+        //asks for bill before tip
+        System.out.println("How many people are in your party?");
+        int people=input.nextInt();
+        System.out.println("How much would you like to tip?");
+        double percentTip=input.nextDouble();
+        percentTip/=100;
+        //divide by 100 to add to 1 and multiply by total
+        double total=(1+percentTip)*bill;
+        //get bill with tip
+        double perPerson=total/people;
+        //what each person has to pay
+        System.out.println("The total bill is $"+total);
+        System.out.println("Each person has to pay a tip of $");
+        System.out.println("Each person will have to pay a total of $"+perPerson+".");
+
+    }
+}
