a9a04278dcebceea2154fdede21e1614ec69e74f
dariyab1 <dariyab@nycstudents.net>
2022-10-07 08:53:04 -0400

    finish

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -15,9 +15,21 @@ public class TipCalculator {
         //get bill with tip
         double perPerson=total/people;
         //what each person has to pay
-        System.out.println("The total bill is $"+total);
-        System.out.println("Each person has to pay a tip of $");
-        System.out.println("Each person will have to pay a total of $"+perPerson+".");
+        double totalTip=total-bill;
+        System.out.printf("The total bill is $ %.2f %n",total);
+        System.out.printf("The total tip is $ %.2f %n",totalTip);
+        System.out.printf("Each person has to pay a tip of $ %.2f %n",(totalTip/people) );
+        System.out.printf("Each person will have to pay a total of $ %.2f %n",perPerson);
 
     }
 }
+    //Your program should prompt the user for the following information:
+        //Total bill
+       // Tip Percentage (in a whole number, without the percentage sign)
+        //Number of people in the party
+
+       // Your program should then calculate the following information and display it to the user (this is slightly different than the information above):
+       // The total tip amount
+       // The total bill including tip
+       // The tip per person
+       // The total per person (bill and tip together)
